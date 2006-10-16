0231db643d7add769482a17fad1ee3d4d5995ca7
mfrasca <>
2006-10-16 07:50:56 +0000

    updated the inline documentation.

--- wmdocklib/__init__.py
+++ wmdocklib/__init__.py
@@ -1,2 +1,12 @@
-from pywmgeneral import *
+"""\
+wmdocklib is a python module that will help you develop WindowMaker dockapps
+in python.  It is mostly a wrapper around the functions from the popular
+wmgeneral.c, but some new functions are added too.
+
+to get help about a contained package, try:
+help(wmdocklib.<name>)
+"""
 from pywmhelpers import *
+from pywmgeneral import *
+
+__all__ = []
